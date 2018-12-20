1b584a58e16404c2cdec348e9d95fcf5f250acaa
Taras Palczynski <tarasikpalczynski@gmail.com>
2018-12-20 14:53:46 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'MDCwebsite',
+    script: './index.js'
+  }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-18-220-23-47.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/webserverInstance.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:zardini123/MDCwebsite.git',
+        path: '/home/ubuntu/MDCwebsite',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+  }
+}

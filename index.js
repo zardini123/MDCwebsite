1eb0c60ae3bfcd2a11b0e0dd2b3a7c58dd644f87
Taras Palczynski <tarasikpalczynski@gmail.com>
2018-12-20 16:27:38 -0500

    Global html header test

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// Command for deploying server after git push:
+//    npm run-script deploy
+
 const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
+
+/*
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
+*/
 
 app.listen(3000, () => console.log('Server running on port 3000'))
---
+++ public/headerBar.js
@@ -0,0 +1,12 @@
+document.write('\
+<!DOCTYPE html>\
+<head>\
+  <link rel="stylesheet" href="index.css">\
+</head>\
+\
+<div class="navbar">\
+  <a href="entries/entry.html">Entry</a>\
+  <a href="#news">News</a>\
+  <a href="#contact">Contact</a>\
+</div>\
+');
